cf111837bb728aba9e9858910ccd176276c4195d
Joaquin Beltran <contact@joaquinmx.com>
2023-11-06 19:30:06 -0600

    refactor: copied AndreaBizzoto 'feature-first' project structure

--- ecommerce_app/lib/main.dart
+++ ecommerce_app/lib/main.dart
@@ -2,13 +2,16 @@ import 'package:ecommerce_app/src/app.dart';
 import 'package:ecommerce_app/src/localization/string_hardcoded.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-//ignore:depend_on_referenced_packages
+// ignore:depend_on_referenced_packages
 import 'package:flutter_web_plugins/url_strategy.dart';
 import 'package:go_router/go_router.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  // turn off the # in the URLs on the web
   usePathUrlStrategy();
+  // ensure URL changes in the address bar when using push / pushNamed
+  // more info here: https://docs.google.com/document/d/1VCuB85D5kYxPR3qYOjVmw8boAGKb7k62heFyfFHTOvw/edit
   GoRouter.optionURLReflectsImperativeAPIs = true;
   // * Register error handlers. For more info, see:
   // * https://docs.flutter.dev/testing/errors
